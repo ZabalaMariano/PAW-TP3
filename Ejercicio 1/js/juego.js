c772cecbde81cb11e8557f87681dc5c1ea28eda5
fcataniae <cataniafrane@gmail.com>
2019-04-17 00:08:29 -0300

    buscaminas completo

--- Ejercicio 1/js/juego.js
+++ Ejercicio 1/js/juego.js
@@ -5,7 +5,7 @@ var window = window || {},
 Juego.nivelActual = {};
 
 
-
+/*METODO DE INICIALIZACION*/
 Juego.armarJuego = function (contenedor) {
   Juego.contenedor = contenedor;
   window.addEventListener("DOMContentLoaded", function () {
@@ -21,18 +21,30 @@ Juego.armarJuego = function (contenedor) {
 }
 
 
+/*METODO PARA INICIALIZAR EL BUSCAMINAS*/
 Juego.generarJuego = function(modo){
   var nivel = Juego.niveles.filter(n => n.modo === modo),
       section = document.querySelector(".juego-buscaminas"),
-      div, cuadrados, asignado, img, ul,li;
+      div, cuadrados, asignado, img, divfila, divjuego, boton;
   Juego.nivelActual = nivel[0];
 
-  ul = document.createElement("ul");
-  section.appendChild(ul);
+  divjuego = document.createElement("div");
+  divjuego.classList.add("contenedor-juego-bm");
+
+  div = document.createElement("div");
+  img = document.createElement("img");
+  img.setAttribute("src","images/play.svg");
+  div.appendChild(img);
+  div.addEventListener("click", Juego.reiniciar);
+
+  div.classList.add("imagen-juego");
+
+  divjuego.appendChild(div);
+
   for(var i = 0; i < Juego.nivelActual.alto; i++){
 
-    li = document.createElement("li");
-    li.classList.add("fila-buscaminas");
+    divfila = document.createElement("div");
+    divfila.classList.add("fila-buscaminas");
     for(var j = 0; j < Juego.nivelActual.ancho; j++){
       div = document.createElement("div");
       div.setAttribute("data-x",j);
@@ -45,12 +57,20 @@ Juego.generarJuego = function(modo){
       div.appendChild(img);
       div.setAttribute("value","blank");
 
-      li.appendChild(div);
+      divfila.appendChild(div);
     }
-    ul.appendChild(li);
+    divjuego.appendChild(divfila);
   }
 
-  cuadrados = document.querySelectorAll("div.cuadrado");
+  boton = document.createElement("input");
+  boton.setAttribute("type","button");
+  boton.setAttribute("value","volver al menu anterior");
+  boton.addEventListener("click",Juego.mostrarMenu);
+
+  divjuego.appendChild(boton);
+
+  section.appendChild(divjuego);
+  cuadrados = document.querySelectorAll("div.fila-buscaminas div.cuadrado");
 
   for(var k = 0; k < Juego.nivelActual.minas; k++){
 
@@ -69,52 +89,57 @@ Juego.generarJuego = function(modo){
       cargarLimites(i, j, Array.prototype.slice.call(cuadrados));
     }
   }
+
+
   document.getElementById("buscaminas-lista-opciones").classList.add("disabled");
 }
 
-
-
-Juego.cargarOpciones = function(modo){
-
-  Juego.generarJuego(modo);
+Juego.mostrarMenu = function(){
+  let juego = document.querySelector("div.contenedor-juego-bm");
+  juego.parentNode.removeChild(juego);
+  let menu = document.getElementById("buscaminas-lista-opciones");
+  menu.classList.remove("disabled");
 }
 
-
-
+/*METODO QUE PARA LOS DIV:CUADRADO*/
 Juego.buscarMina = function(e){
   var target = e.target.localName == "img" ? e.target.parentNode: e.target ;
 
   if(target.getAttribute("value") === "bomb"){
       target.classList.remove("sin-descubrir");
-      alert("perdiste");
+      perdiste();
 
   }
   else
   {
       var x = parseInt(target.getAttribute("data-x")),
           y = parseInt(target.getAttribute("data-y")),
-          cuadrados = Array.prototype.slice.call(document.querySelectorAll("div.cuadrado"));
+          cuadrados = Array.prototype.slice.call(document.querySelectorAll("div.fila-buscaminas div.cuadrado"));
       descubrir(x, y, cuadrados);
 
       if(juegoTerminado(cuadrados)){
-        alert("ganaste");
+        ganaste();
       }
 
   }
 
 }
 
+
+/*FUNCION PARA DETERMINAR SI YA DESCUBRIO TODOS LOS CUADRADOS*/
 function juegoTerminado(array){
   const ar = array.filter( c => c.getAttribute("value") == "blank" || (c.classList.contains("sin-descubrir") && c.getAttribute("value") != "bomb"));
   return ar.length == 0;
 }
 
+
+/*FUNCION PARA DESCUBRIR LOS CUADRADOS HASTA QUE LLEGA A UN LIMITE*/
 function descubrir(x,y,array){
-  console.log("descubrir "+ x +" "+ y  );
+
   if(!(x < 0 || x >= Juego.nivelActual.ancho ||
       y < 0 || y >= Juego.nivelActual.alto))
   {
-    var  actual = array.filter( c => c.getAttribute("data-x") == x && c.getAttribute("data-y") == y)[0];
+    var  actual = document.querySelector("div.fila-buscaminas div[data-x='"+x+"'][data-y='"+y+"']");
     if(actual.getAttribute("value") == "blank"){
 
       actual.setAttribute("value","descubierto");
@@ -132,20 +157,22 @@ function descubrir(x,y,array){
       descubrir(x-1, y, array);
 
     }else if(actual.getAttribute("value") == "limite"){
-      console.log("asdasdas");
+
       actual.classList.remove("sin-descubrir");
       actual.classList.add("descubierto");
     }
   }
 }
 
+/*FUNCION PPARA CARGAR LOS LIMITES CON LAS IMAGENES DE CUANTAS BOMBAS
+ALEDAÑAS TIENE */
 function cargarLimites(x,y,array){
 
   if(!(x < 0 || x >= Juego.nivelActual.ancho ||
       y < 0 || y >= Juego.nivelActual.alto))
   {
 
-  var  actual = array.filter( c => c.getAttribute("data-x") == x && c.getAttribute("data-y") == y)[0];
+  var  actual = document.querySelector("div.fila-buscaminas div[data-x='"+x+"'][data-y='"+y+"']");
   if(actual.getAttribute("value") != "bomb"){
         let minas = minasAledañas(x+1, y+1, array)+
                     minasAledañas(x-1, y+1, array)+
@@ -166,41 +193,36 @@ function cargarLimites(x,y,array){
 
 
 
-
+/*FUNCION PARA DETERMINAR SI UN CUADRADO ES BOMBA O NO*/
 function minasAledañas(x,y,array){
-  console.log(x + "  "+ y);
-    if( x < 0 || x >= Juego.nivelActual.ancho  ||
-        y < 0 || y >= Juego.nivelActual.alto ){
 
-          return 0;
+  if( x < 0 || x >= Juego.nivelActual.ancho  ||
+      y < 0 || y >= Juego.nivelActual.alto ){
+
+        return 0;
+      }
+      else{
+          var  actual = document.querySelector("div.fila-buscaminas div[data-x='"+x+"'][data-y='"+y+"']");
+          if(actual.getAttribute("value") == "bomb"){
+            return 1;
+          }else{
+            return 0;
         }
-        else{
-            var  actual = array.filter( c => c.getAttribute("data-x") == x && c.getAttribute("data-y") == y)[0];
-
-            if(actual.getAttribute("value") == "bomb"){
-              return 1;
-            }else{
-              return 0;
-          }
   }
 }
 
 
 
-
+/*METODO QUE VA CON LOS ITEMS DE LA LISTA*/
 Juego.seleccion = function(e){
   if(e.target && e.target.matches("li.opcion")){
-
-    if(e.target.innerHTML != "Personalizado")
       Juego.generarJuego(e.target.innerHTML);
-    else
-      Juego.cargarOpciones(e.target.innerHTML);
   }
 }
 
 
 
-
+/*GENERO EL MENU CON LOS MODOS DE JUEGO*/
 Juego.generarMenuSeleccion = function(section){
 
   var ul = document.createElement("ul");
@@ -222,4 +244,34 @@ Juego.generarMenuSeleccion = function(section){
   });
 
   section.appendChild(ul);
+}
+
+/*FUNCION PARA REINICIAR EL JUEGO APRETANDO LA CARITA*/
+Juego.reiniciar = function(){
+  let juego = document.querySelector("div.contenedor-juego-bm");
+  juego.parentNode.removeChild(juego);
+  Juego.generarJuego(Juego.nivelActual.modo);
+}
+
+/*FUNCION PARA DESHABILITAR EL EVENTLISTENER*/
+function inhabilitarOpcionYmostrar(){
+
+  let cuadrados = document.querySelectorAll("div.fila-buscaminas div.cuadrado");
+
+  cuadrados.forEach( c => {
+    c.removeEventListener("click",Juego.buscarMina)
+    c.classList.remove("sin-descubrir");
+  });
+
+}
+
+/*FUNCION CON LOGICA CUANDO PIERDE*/
+function perdiste(){
+  document.querySelector("div.imagen-juego img").setAttribute("src", "images/lose.svg");
+  inhabilitarOpcionYmostrar();
+}
+/*FUNCION CON LOGICA CUANDO GANA*/
+function ganaste(){
+  document.querySelector("div.imagen-juego img").setAttribute("src", "images/win.svg");
+  inhabilitarOpcionYmostrar();
 }
